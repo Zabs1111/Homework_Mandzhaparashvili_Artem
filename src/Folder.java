870b8def0ac83cc8fbacbadc2b999a64e43feb7b
Zabs1111 <manjikgamer@gmail.com>
2026-07-12 23:57:41 +0300

    Update Folder.java

--- src/Folder.java
+++ src/Folder.java
@@ -2,13 +2,14 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-public record Folder(List<String> documents) {
+public final class Folder {
+    private final List<String> documents;
+
     public Folder(List<String> documents) {
         this.documents = new ArrayList<>(documents);
     }
 
-    @Override
-    public List<String> documents() {
+    public List<String> getDocuments() {
         return Collections.unmodifiableList(documents);
     }
 }
